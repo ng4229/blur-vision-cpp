fb76facdba2c626e8a0d58c786b308d801369b2c
Mangirl <ngseo@iglim.net>
2025-02-05 01:14:09 +0900

    feat: ImageObject 클래스 구현
    멤버 변수 : m_kWidth, m_kHeight, m_vecData
    메서드 : convertToMat, getImageSize
    생성자 : 기본 생성자, Mat을 매개변수로 받는 생성자

---
+++ BlurManager/ImageObject.h
@@ -0,0 +1,46 @@
+#pragma once
+#include <vector>
+#include <opencv2/opencv.hpp>
+
+class ImageObject
+{
+private:
+	uint32_t m_kWidth;
+	uint32_t m_kHeight;
+	std::vector<uint8_t> m_vecData;
+
+public:
+	// 기본 생성자 (이미지 크기 0 x 0 으로 기본 셋팅)
+	ImageObject() : m_kWidth(0), m_kHeight(0) {}
+	
+	// 매개변수 : cv::Mat
+	// 초기화할 image 받는 생성자
+	ImageObject(const cv::Mat& image) 
+	{
+		if (image.type() != CV_8UC1)	// Grayscale 외의 이미지는 Exception 발생
+		{
+			throw std::invalid_argument("Only grayscale images are supported.");
+		}
+		m_kWidth = image.cols;
+		m_kHeight = image.rows;
+		m_vecData.assign(image.data, image.data + (m_kHeight * m_kWidth));
+	}
+	
+	// 기본 소멸자
+	~ImageObject() {};
+
+	// return type : cv::Mat
+	// 기능 : Buffer data를 cv::Mat으로 변환 메서드
+	cv::Mat convertToMat()
+	{
+		return cv::Mat(m_kHeight, m_kWidth, CV_8UC1, (void*)m_vecData.data()).clone();
+	}
+
+	// return type : std::pair<uint32_t, uint32_t>
+	// 기능 : 이미지 크기 반환 메서드 
+	std::pair<uint32_t, uint32_t> getImageSize()
+	{
+		return std::pair<uint32_t, uint32_t>(m_kHeight, m_kWidth);
+	}
+};
+
